e55a5f5ed0f156dd322c5ea332192d3207015cc5
MartinHeinz <martin7.heinz@gmail.com>
2022-12-19 15:07:17 +0100

    Lock Kubernetes version to 1.25.0-00.

--- kube-tools.sh
+++ kube-tools.sh
@@ -2,7 +2,7 @@ sudo su -
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubelet kubeadm kubectl
+apt-get install -y kubelet=1.25.0-00 kubeadm=1.25.0-00 kubectl=1.25.0-00
 apt-mark hold kubelet kubeadm kubectl
 
 echo 'source <(kubectl completion bash)' >>~/.bashrc
