7d95c291a48f24371c46726ae5a4f291c5d2ea75
MartinHeinz <martin7.heinz@gmail.com>
2023-05-21 17:15:09 +0200

    Make Kine into systemd service.

---
+++ setup-kine.sh
@@ -0,0 +1,41 @@
+sudo su -
+
+wget -q https://github.com/k3s-io/kine/releases/download/v0.10.1/kine-amd64
+install -m 755 kine-amd64 /usr/local/sbin/kine && rm kine-amd64
+
+apt -y install postgresql postgresql-contrib
+systemctl start postgresql.service
+
+# Generate self signed root CA cert
+openssl req -addext "subjectAltName = DNS:localhost" -nodes -x509 -newkey rsa:2048 -keyout ca.key -out ca.crt -subj "/CN=localhost"
+
+# Generate server cert to be signed
+openssl req -addext "subjectAltName = DNS:localhost" -nodes -newkey rsa:2048 -keyout server.key -out server.csr -subj "/CN=localhost"
+
+# Sign the server cert
+openssl x509 -extfile <(printf "subjectAltName=DNS:localhost") -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
+
+chmod og-rwx ca.key
+chmod og-rwx server.key
+
+cp {server.crt,server.key,ca.crt} /var/lib/postgresql/
+chown postgres.postgres /var/lib/postgresql/server.key
+
+sed -i -e "s|ssl_cert_file.*|ssl_cert_file = '/var/lib/postgresql/server.crt'|g" /etc/postgresql/14/main/postgresql.conf
+sed -i -e "s|ssl_key_file.*|ssl_key_file = '/var/lib/postgresql/server.key'|g" /etc/postgresql/14/main/postgresql.conf
+sed -i -e "s|#ssl_ca_file.*|ssl_ca_file = '/var/lib/postgresql/ca.crt'|g" /etc/postgresql/14/main/postgresql.conf
+
+# Edit /etc/postgresql/14/main/pg_hba.conf
+# Change line to 'host    all             all             127.0.0.1/32            trust'
+
+sed -i -e "s|host    all             all             127.0.0.1/32            scram-sha-256|host    all             all             127.0.0.1/32            trust|g" /etc/postgresql/14/main/pg_hba.conf
+systemctl restart postgresql.service
+psql -U postgres -p 5432 -h 127.0.0.1 -c "ALTER ROLE postgres WITH PASSWORD 'somepass';"
+sed -i -e "s|host    all             all             127.0.0.1/32            trust|host    all             all             127.0.0.1/32            scram-sha-256|g" /etc/postgresql/14/main/pg_hba.conf
+
+systemctl restart postgresql.service
+
+cp kine.sh /usr/bin/kine.sh
+cp kine.service /lib/systemd/system/kine.service
+
+systemctl start kine.service
